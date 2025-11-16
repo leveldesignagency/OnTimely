3b4c369a39db91417d5489eccbafee0197914c50
Charles Morgan <charles.morgan@example.com>
2025-11-16 21:59:46 +0000

    Revert to module.exports format to match other API endpoints

--- api/request-password-reset.js
+++ api/request-password-reset.js
@@ -9,7 +9,7 @@ if (!supabaseUrl || !supabaseServiceKey) {
 
 const supabase = createClient(supabaseUrl, supabaseServiceKey);
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
